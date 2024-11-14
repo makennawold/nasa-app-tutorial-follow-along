647800378fa67d457adc35beb44de1c5c32b5320
makennawold <makennawold@gmail.com>
2024-11-14 11:51:55 -0700

    added components

---
+++ nasa-picks-tut-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Main from "./components/Main"
+import Footer from './components/Footer'
+import SideBar from './components/SideBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
